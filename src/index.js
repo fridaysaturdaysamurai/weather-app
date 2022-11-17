2fdd13af0a0fd569a0a4adac31bf4bdad0bf2e3d
Emil Gashimov <gashimow19998@gmail.com>
2022-11-17 22:00:16 +0400

    fixed temperature

--- src/index.js
+++ src/index.js
@@ -2,25 +2,23 @@ const inputSearch = document.querySelector('input');
 const searchBtn = document.querySelector('.search__btn');
 const weatherInfo = document.querySelector('.weather__info');
 
-let inputValue = inputSearch.value;
+const getWeatherData = async () => {
+    let inputValue = inputSearch.value;
 
-const getWeatherData = async (inputValue) => {
     try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputSearch.value}&appid=426e990cd458d405cfbad4f07392b191`);
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=426e990cd458d405cfbad4f07392b191&units=metric`);
         const data = await response.json();
 
         weatherInfo.innerHTML = `
             <h1 class="city__name">Weather in ${data.name}:</h1>
-            <p class="temperature">Weather temperature: ${(data.main.temp - 273).toFixed(0)} (°C).</p>
+            <p class="temperature">Weather temperature: ${data.main.temp.toFixed(0)} (°C).</p>
             <p class="cloudiness">Cloudiness: ${data.weather[0].description}.</p>
             <p class="status">Wind: ${data.wind.speed} m/s.</p>
         `
-
+        
     } catch(error) {
         console.log('Error >>>', error)
     }
 }
 
-searchBtn.addEventListener('click', () => {
-    getWeatherData(inputValue);
-})
+searchBtn.addEventListener('click', getWeatherData);
